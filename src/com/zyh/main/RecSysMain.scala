4578bee41f3d9030e06365aa43e49e9091463abf
yuehui <18205157336@163.com>
2018-04-06 19:27:41 +0800

    change string to int

--- src/com/zyh/main/RecSysMain.scala
+++ src/com/zyh/main/RecSysMain.scala
@@ -5,92 +5,73 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext 
 
 object RecSysMain {
-
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf() 
     conf.setAppName("RecSys-SPARKAPP") 
-        .setMaster("spark://35.201.136.180:7077")
-        .set("spark.executor.memory", "12g")
-        .set("spark.executor.cores", "2")
-        .set("spark.driver.memory", "20g")
+        .setMaster("spark://master:7077")
+        .set("spark.executor.memory", "2g")
+        .set("spark.executor.cores", "1")
+        .set("spark.driver.memory", "2g")
     val sc = new SparkContext(conf) 
-    sc.addJar("/home/yuehui/workspace/spark/RecSys/bin/RecSys.jar")
+    sc.addJar("/home/yuehui/git/RecSys/bin/RecSys.jar")
     //最小支持度
-    val minSupport = 0.05
+    val minSupport = 0.15
     //最小置信度
-    val minConfidence = 0.6
+    val minConfidence = 0.8
     //数据分区数
     val numPartitions = 12
     //hdfs input url 
-    val inputUrl = "hdfs://35.201.136.180:9000/data/"
+    val inputUrl = "hdfs://master:9000/data/"
     //hdfs out url 
-    val outputUrl = "hdfs://35.201.136.180:9000/out/main/"
+    val outputUrl = "hdfs://master:9000/out/main/"
     //读取数据
     val data = sc.textFile(inputUrl + "D.dat",numPartitions)
     val user = sc.textFile(inputUrl + "U.dat",numPartitions)
-    
-    val transactions: RDD[Array[String]] = data.map(line => line.trim.split(' '))
+    //转换为Int数组，并去重，更节省内存空间
+    val transactions = data.map(line => line.trim.split(" ").map(_.toInt).distinct)
     transactions.cache()
+    
+    
+    //用户购买记录Set
+    val usrSet = user.map(line => line.trim.split(" ").map(_.toInt).toSet)
+    usrSet.cache()
     //创建一个FPGrowth的算法实列
     //设置计算的最小支持度和数据分区
-    
     val fpg = new FPGrowth().setMinSupport(minSupport).setNumPartitions(numPartitions)
-    
+    //获得FPGrowth计算结果
     val model = fpg.run(transactions)
-    
     //收集频繁项集
-    val fre = model.freqItemsets.map(itemset => (itemset.freq,itemset.items.mkString(" ")))
+    val fre = model.freqItemsets.map(itemset => (itemset.freq, itemset.items))
     fre.cache()
+    //频繁项集升序排序
     val freq = fre.sortByKey().collect
-    val resfreq = sc.makeRDD(freq.map(u => u._2))
-    //val resfreq = fre.map(x => x._2)
-    //resfreq.foreach(f => println("resfreq => "+f))
-    resfreq.saveAsTextFile(outputUrl+"resfreq")
+    val resfreq = sc.makeRDD(freq.map(u => u._2.mkString(" ")))
+    resfreq.saveAsTextFile(outputUrl+"freqItemsets")
     
     //推荐规则[前项, (后项, 置信度)]
-    val AllAssociationRules = 
-      model.generateAssociationRules(minConfidence)
-      .map(x => (x.antecedent.toSet,(x.consequent.mkString(" "),x.confidence)))
-      
-    val rulesList =AllAssociationRules
-      .combineByKey(
-          List(_),
-          (x:List[(String, Double)], y:(String, Double)) => y :: x, 
-          (x:List[(String, Double)], y:List[(String, Double)]) => x ::: y)//不知道combineByKey是否平均分配到每个分区.repartition(numPartitions)
+    val AllAssociationRules = model.generateAssociationRules(minConfidence).map(x => (x.antecedent.toSet, (x.consequent.toSet, x.confidence)))
     
-    //保存关联规则与置信度
-    rulesList.map(rule => (rule._1.mkString("[", ",", "]"), rule._2.mkString("[", " | ", "]")))
-     .saveAsTextFile(outputUrl+"rulesList") 
-     
-    //置信度最高的规则，置信度相同则合并
-    val bestRules = 
-      rulesList.map(rule => 
-        (rule._1, (rule._2).toList.reduceLeft((a, b) => if(a._2 > b._2) a else if(a._2 == b._2) (a._1 + " " + b._1, a._2) else b)))
+    //根据前项聚合产生的规则 [前项，[(后项，置信度)]]
+    //不知道combineByKey是否平均分配到每个分区.repartition(numPartitions)
+    val rulesList =AllAssociationRules.combineByKey(List(_), (x:List[(Set[Int], Double)], y:(Set[Int], Double)) => y :: x, (x:List[(Set[Int], Double)], y:List[(Set[Int], Double)]) => x ::: y)
+    //保存关联规则与置信度[a,b] --> [ c : 0.9 ] | [ d : 0.8 ]
+    rulesList.map(rule => (rule._1.mkString("[", ",", "]") + " --> " + rule._2.map(x => (x._1 + " : " + x._2.toString())).mkString("[ ", " | ", " ]"))).saveAsTextFile(outputUrl+"rulesList") 
+
+    //计算置信度最高的规则，置信度相同则合并
+    val bestRules = rulesList.map(rule => (rule._1, rule._2.reduceLeft((a, b) => if(a._2 > b._2) a else if(a._2 == b._2) (a._1 ++ b._1, a._2) else b)))
     bestRules.cache()
     //保存关联规则中置信度最高的
-    bestRules.map(rule => rule._1.mkString("[", ",", "]") + " --> " + rule._2._1.split(" ").mkString("[", ",", "]") + " : " + rule._2._2)
-      .saveAsTextFile(outputUrl + "bestRules") 
-    //用户购买记录Set
-    val usrSet = user.map(line => (line.trim.split(" ").toSet))
+    bestRules.map(rule => rule._1.mkString("[", ",", "]") + " --> " + rule._2._1.mkString("[", ",", "]") + " : " + rule._2._2).saveAsTextFile(outputUrl + "bestRules") 
+   
     //收集，便于遍历
     val rules = bestRules.collect()
     //计算用户推荐项
-    val userRecItems = usrSet.map(
-        uSet => (
-            rules.filter(x => x._1.subsetOf(uSet))
-              .map(x => (x._2._1.split(" ").toSet, x._2._2))
-              .reduce((x, y) => if(x._2 > y._2) x else if (x._2 == y._2) (x._1 ++ y._1, x._2) else y)._1, uSet
-              )
-            ).map(u => (u._1.filter(x => !u._2.contains(x)), u._2))
+    val userRecItems = usrSet.map(uSet => (rules.filter(x => x._1.subsetOf(uSet)).map(x => (x._2._1, x._2._2)).reduce((x, y) => if(x._2 > y._2) x else if (x._2 == y._2) (x._1 ++ y._1, x._2) else y)._1, uSet)).map(u => (u._1.filter(x => !u._2.contains(x)), u._2))
     userRecItems.cache()
     //保存格式化
-    if(userRecItems.isEmpty()){
-      println("userRecItems is Empty")
-    }else {
-      val result = userRecItems.map(item => (item._1.toList.mkString("[", ",", "]") +" for user : " + item._2.toList.mkString("[", ",", "]")))
-      result.cache()
-      result.saveAsTextFile(outputUrl+"userRecItems")
-    }
+    val result = userRecItems.map(item => (item._1.mkString("[", ",", "]") +" for user : " + item._2.mkString("[", ",", "]")))
+    result.cache()
+    result.saveAsTextFile(outputUrl+"userRecItems")
     
     println("end of RecSys")
   }
